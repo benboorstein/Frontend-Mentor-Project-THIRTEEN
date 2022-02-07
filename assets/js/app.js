5891fe29bf7543049d6f141f6e8f26ecb74a432a
Ben <benboorstein@gmail.com>
2022-02-07 18:09:44 -0500

    changes to HTML, CSS, JS.  JS question.

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,59 @@ See an updated progress bar and total money raised based on their pledge total a
 See the number of total backers increment by one after confirming a pledge
 Toggle whether or not the product is bookmarked
 */
+
+
+
+
+// trigger "Back this project" modal
+document.querySelector('.back-proj-btn').addEventListener('click', () => {
+    document.querySelector('.back-proj-modal').style.display = 'block'
+})
+
+// close "Back this project" modal
+document.querySelector('.close-modal').addEventListener('click', () => {
+    document.querySelector('.back-proj-modal').style.display = 'none'
+})
+
+
+
+
+
+
+
+
+
+// trigger the selected pledge
+document.querySelectorAll('.check-to-choose').forEach(checkbox => {
+    checkbox.addEventListener('click', () => {
+        if (checkbox.checked) {
+            document.querySelector('.enter-pledge').classList.remove('is-hidden') // PROB LINE // What I WANT is to 'get' the '.enter-pledge' element that corresponds to the specific checkbox being clicked (checked in this case)
+         } else {
+            document.querySelector('.enter-pledge').classList.add('is-hidden') // PROB LINE // What I WANT is to 'get' the '.enter-pledge' element that corresponds to the specific checkbox being clicked (unchecked in this case)
+         }
+    })
+    // document.querySelectorAll('.enter-pledge').forEach(section => {
+        
+    // })      
+})
+
+
+
+
+
+
+
+
+
+// trigger "Thanks..." modal
+document.querySelectorAll('.pledge-continue-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.querySelector('.back-proj-modal').style.display = 'none'
+        document.querySelector('.thank-you-modal').style.display = 'block'
+    })
+})
+
+// close "Thanks..." modal
+document.querySelector('.got-it-btn').addEventListener('click', () => {
+    document.querySelector('.thank-you-modal').style.display = 'none'
+})
